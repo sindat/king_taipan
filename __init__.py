694125b8ec8f6218000b2e30527f12e9dbdbadc3
David Vondrovic <david.vondrovic@gmail.com>
2019-05-11 18:08:17 +0200

    base structure import

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# THIS IS THE MAIN PACKAGE
+# the __all__ list specifies which modules to import from this main package
+__all__ = ['client', 'server', 'core', 'modules']
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__github__ = 'https://github.com/sindat/king_taipan/'
