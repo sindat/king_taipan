59963bcce0150c983e2a69493906dc2383d614eb
Vondrovic <david.vondrovic@sap.com>
2019-05-19 19:44:22 +0200

    main function and the cc class update

--- server.py
+++ server.py
@@ -19,8 +19,6 @@ import struct
 import random
 # retrieve source code of a method, examine contents of a class, display detailed traceback
 import inspect
-# parser for command line options and arguments
-import argparse
 import datetime
 # construct higher level threading interfaces
 import threading
@@ -32,11 +30,14 @@ import collections
 # WEB SERVER MODULES
 import http.server 
 import socketserver
+# PARSER FOR COMMAND LINE ARGUMENTS 
+import argparse
 
+"""
 PORT = 6969
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer('127.0.0.1',PORT), Handler) as httpd:
+with socketserver.TCPServer(("127.0.0.1",PORT), Handler) as httpd:
     print("SERVER RUNNING AT PORT ", PORT)
     httpd.serve_forever()
 
@@ -50,13 +51,20 @@ if sys.version_info[0] > 2:
     sys.path.append('serverModules')
     sys.path.append('clientModules')
 
+"""
+
 # SERVER MODULES IMPORT 
+
+"""
 import serverModules.util as util
 import serverModules.database as database
+"""
 #import serverModules.security as security
 
 
 # importing video processing package - cv2 (OpenCV)
+
+"""
 try: 
     import cv2
 except ImportError:
@@ -67,47 +75,75 @@ try:
     import colorama
 except ImportError:
     sys.exit("CRITICAL ERROR: PACKAGE 'colorama' IS REQUIRED")
+"""
 
 
 
 ###### MAIN FUNCTION ######
 def main():
+    
+    # DEFINE THE PARSER VARIABLE 
     parser = argparse.ArgumentParser(
-        prog = 'server.py'
+        prog = 'server.py',
         description = "CC command line server"
     )
-    # FILL ARGUMENT PARSER WITH INFORMATION
+    # HOST INFO ARGUMENT
     parser.add_argument(
         '--host',
         action='store',
-        type='str',
+        type=str,
         default='127.0.0.1',
-        help='Please enter your server hostname or IP address where server will run'
+        help='Please enter your server IP address where server will run'
     )
-
+    # PORT INFO ARGUMENT 
     parser.add_argument(
         '--port',
         action='store',
-        type='int',
+        type=int,
         default=6969,
         help="Specify server listening port"
     )
 
     # SPECIFY DATABASE - LINKED TO THE DATABASE MODULE
     # I CAN LINK DIRECTLHY TO THE DATABASE BECAUSE IT'S REFERENCED IN THE PYTHONPATH AND MODULE IMPORT, IMPORTED AS "database"
+    
+    """
     parser.add_argument(
         '--database',
         action='store',
-        type='str',
-        default='database.mainDB',
+        type=str,
+        default='database.db',
         help='the linked SQLite database'
-    )
+    )  
+    """
 
-###### THE KEY CLASS ######
+   
+    options = parser.parse_args()
+    globals()['cc'] = CC(host=options.host, port=options.port)
+
+
+
+
+# COMMAND & CONTROL (CC) 
+# RETURNS AN INSTANCE OF THE CC SERVER
+# WILL BE INSTANTIATED IN THE main() FUNCTION - WRITE THE run() FUNCTION FOR IT TO LAUNCH THE TERMINAL
 class CC():
-    def __init__(self, host='127.0.0.1', port=6969):
-        # server database attribute
-        self._database = db
-        # listening server socket bound to port 
-        self.socket = self._socket(port)
+    def __init__(self, host='127.0.0.1', port=6969, db=":memory:"):
+        """
+        CREATES A NEW COMMAND & CONTROL SERVER, FIRE IT UP WITH SPECIFIED PARAMETERS
+        INSTANTIATED WITH THE main() FUNCTION
 
+        DB IS TAKEN FROM :memory:, AS IN THE CURRENT SESSION
+        """
+        
+
+    def run(self):
+        """
+        RUN THE COMMAND & CONTROL SERVER TERMINAL
+        ALSO IT FIRES UP THE PYTHON HTTP WEB SERVER serve_forever 
+        OPENS UP A TERMINAL READY TO RECEIVE COMMANDS, ALSO REACTS TO KEYBOARD INTERRUPTS
+        """
+
+# RUN MAIN FUNCTION ONLY WHEN THIS PROGRAM IS BEING EXECUTED, NOT IMPORTED 
+if __name__ == '__main__':
+    main()
